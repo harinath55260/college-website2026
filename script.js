06a78fb658ca319bac8a6a1b4a3893872beabbce
harinath55260 <hariharinath55260@gmail.com>
2026-03-23 18:59:33 +0530

    Refactor showPage function and add default page display

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ p.style.display = "none";
 document.getElementById(page).style.display = "block";
 
 }
+// Default home page show
+document.addEventListener("DOMContentLoaded", function(){
+    showPage("home");
+});
 
 
 // FORM SUBMIT
